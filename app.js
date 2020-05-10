08101c4c47136a3903c862334a0e906de7872f94
green4450 <t.green7244@student.sbccd.edu>
2020-05-10 11:33:00 +0530

    asa

--- app.js
+++ app.js
@@ -192,7 +192,7 @@ app.get('/internship-register/:email', (req, res) => {
         from: 'akashpandey.geu@gmail.com',
         to: email,
         subject: `Congratulations Account Has Been Successfully Created`,
-        text: `Account for ${email} has been successfully Registered Plse Verify Your account by clicking on this link.please click on the link to verfiy yourself http://localhost/internship_go/internship/module_ulb/activate_account.php?email=${$email}.`
+        text: `Account for ${email} has been successfully Registered Plse Verify Your account by clicking on this link.please click on the link to verfiy yourself http://localhost/internship_go/internship/module_ulb/activate_account.php.`
     };
     transporter.sendMail(mailOption, (err, data) => {
         if (err) throw res.send(err);
